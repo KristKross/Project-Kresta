4e8d6f04afc3cd5ce89af223e8d2a6989c6167e3
Kristian Demonteverde <kristkross30@gmail.com>
2025-04-14 05:10:36 +0400

    updated webpack to dynaimcally switch between minicss and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const MODE = process.env.NODE_ENV || 'development'; 
 
 module.exports = {
-    mode: 'development',
+    mode: MODE,
     entry: {
         main: './src/js/main.js',
     },
     output: {
         filename: 'js/[name].bundle.js',
-        path: path.resolve(__dirname, 'public'),
-        publicPath: '/',
+        path: path.resolve(__dirname, 'public'), 
+        publicPath: '/', 
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    MODE === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', 
+                    'css-loader', 
+                    'sass-loader', 
                 ],
             },
         ],
     },
+    plugins: [
+        ...(MODE === 'production' ? [
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css',
+            }),
+        ] : []),
+    ],
     devServer: {
-        static: path.resolve(__dirname, 'public',),
+        static: path.resolve(__dirname, 'public'),
         port: 3000,
         open: true,
     },
