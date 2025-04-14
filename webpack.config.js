c6eb80b5b1bb38a1565e617973fe398a64124367
Kristian Demonteverde <kristkross30@gmail.com>
2025-04-14 13:08:46 +0400

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
 const path = require('path');
+const dotenv = require('dotenv');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+dotenv.config();
 const MODE = process.env.NODE_ENV || 'development'; 
 
 module.exports = {
     mode: MODE,
     entry: {
-        main: './src/js/main.js',
+        main: "./src/js/main.js",
     },
     output: {
-        filename: 'js/[name].bundle.js',
-        path: path.resolve(__dirname, 'public'), 
+        filename: "js/[name].[contenthash].js",
+        path: path.resolve(__dirname, 'dist'), 
         publicPath: '/', 
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html', 
+            template: './src/index.html',
+            inject: 'body',
+            chunks: ['main'],
+        }),
+        ...(MODE === 'production' ? [
+            new MiniCssExtractPlugin({
+                filename: "css/[name].[contenthash].css",
+            }),
+        ] : []),
+    ],
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MODE === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', 
+                    'css-loader',
+                ],
+            },
             {
                 test: /\.scss$/,
                 use: [
@@ -25,16 +48,13 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        ...(MODE === 'production' ? [
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].css',
-            }),
-        ] : []),
-    ],
     devServer: {
-        static: path.resolve(__dirname, 'public'),
+        static: path.resolve(__dirname, 'dist'),
         port: 3000,
         open: true,
+        historyApiFallback: true,
+        watchFiles: [
+            './src/**/*.html'
+        ],
     },
 };
