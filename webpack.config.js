786028665229c01cc57ed2ad63968e685eec184b
Kristian Demonteverde <kristkross30@gmail.com>
2025-04-17 14:38:39 +0400

    removed file loader

--- webpack.config.js
+++ webpack.config.js
@@ -54,16 +54,13 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                use: [
-                    'file-loader',
-                ],
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name].[hash][ext]', 
                 },
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i, 
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource', 
                 generator: {
                     filename: 'fonts/[name].[hash][ext]', 
