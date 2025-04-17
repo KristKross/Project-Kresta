f30ba800a4fc91552f712a44b0f2df2e3033e3ae
Kristian Demonteverde <kristkross30@gmail.com>
2025-04-17 14:51:15 +0400

    changed navbar image to be dynamic

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
 import '../scss/main.scss';
+import logo from '../assets/logo.png';
 
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.createElement('header');
     fetch('../templates/navbar.html')   
         .then(response => response.text())
-        .then(data => header.innerHTML = data)
-        .catch(error => console.error('Error fetching navbar:', error));
-    document.body.insertBefore(header, document.body.firstChild);
+        .then(data => {
+            header.innerHTML = data;
+            document.body.insertBefore(header, document.body.firstChild);
+
+            const logoImg = document.querySelector('.logo');
+            if (logoImg) {
+                logoImg.src = logo;
+            } else {
+                console.error('Logo image not found');
+            }
+        })
+    .catch(error => console.error('Error fetching navbar:', error));
 });
