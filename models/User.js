cb4097636ec876de065065544298cf46aa4c17f3
Kristian Demonteverde <kristkross30@gmail.com>
2025-06-02 09:30:55 +0400

    added removal of the db after 2 months

--- models/User.js
+++ models/User.js
@@ -8,4 +8,15 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
+userSchema.pre('remove', async function(next) {
+  await mongoose.model('Workspace').deleteMany({ owner: this._id });
+  await mongoose.model('Task').deleteMany({ owner: this._id });
+  await mongoose.model('Notification').deleteMany({ user: this._id });
+  await mongoose.model('Social').deleteMany({ userId: this._id });
+  await mongoose.model('Premium').deleteMany({ userId: this._id });
+  next();
+});
+
+mongoose.model('User').collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 30 * 2 });
+
 module.exports = mongoose.model("User", userSchema);
